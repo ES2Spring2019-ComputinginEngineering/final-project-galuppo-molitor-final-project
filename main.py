773dfb0269b559737f001741b91c13346f4329cb
agaluppo <46977185+agaluppo@users.noreply.github.com>
2019-04-16 15:56:33 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 """This is the python file that your instructors will run to test your code
 make sure it runs correctly when someone downloads your repository. You 
-might want to test this on a classmates computer to be sure it works!""
+might want to test this on a classmates computer to be sure it works!"""
 
 # This files should not contain any function defitions
 
 
 # IMPORT STATEMENTS
-
-
+from DataParsing import * 
+from Functions import *
 
 
 # DEMONSTRATION CODE
+print("The correlation between birth weight and the 9 risk factors")
+graphData(Age, BWT, Low)
 
 
