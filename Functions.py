a08d75a56e62f67ad2ff884ca7248e1e2907a934
agaluppo <46977185+agaluppo@users.noreply.github.com>
2019-04-22 16:20:29 -0400

    Add files via upload

--- Functions.py
+++ Functions.py
@@ -11,6 +11,7 @@ import numpy as np
 from matplotlib import pylab
 from numpy import arange, array, ones
 from scipy import stats 
+from astropy.table import Table
 
 from DataParsing import * 
 Low, Age, LWT, Race, Smoker, PTL, Hypertension, UI, FTV, BWT = readDataFile()
@@ -20,6 +21,7 @@ HypertensionNeg_BW, HypertensionPos_BW = List_Creation2(HypertensionNeg_BW, Hype
 FTV_0, FTV_1, FTV_2, FTV_3, FTV_4 = List_Creation5(FTV_0, FTV_1, FTV_2, FTV_3, FTV_4, FTV)
 UINeg, UIPos = List_Creation2(UINeg, UIPos, UI)
 
+#DATA VISUALIZATION FUNCTIONS - individual risks
 def graphNumerical(x_values, y_values, classification, title): 
     plt.figure
     plt.title(title)
@@ -31,15 +33,15 @@ def graphNumerical(x_values, y_values, classification, title):
     plt.plot(x_values, line)
     plt.show()
     
-def Histogram_4(List1, List2, List3, List4, names, bins):
-    colors = ['#E69F00', '#56B4E9', '#F0E442', '#009E73']
-    plt.hist([List1, List2, List3, List4], bins = bins, color = colors, label = names)
-    plt.legend()
-    
-def Histogram_2(List1, List2, names, bins): 
-    colors = ['#F0E442', '#009E73'] 
-    plt.hist([List1, List2], bins=bins, color = colors, label = names)
-    plt.legend()
+#def Histogram_4(List1, List2, List3, List4, names, bins):
+#    colors = ['#E69F00', '#56B4E9', '#F0E442', '#009E73']
+#    plt.hist([List1, List2, List3, List4], bins = bins, color = colors, label = names)
+#    plt.legend()
+#    
+#def Histogram_2(List1, List2, names, bins): 
+#    colors = ['#F0E442', '#009E73'] 
+#    plt.hist([List1, List2], bins=bins, color = colors, label = names)
+#    plt.legend()
 
 def DensityPlot_2(List1, List2, Label1, Label2, Title):
     plt.figure()
@@ -69,5 +71,42 @@ def DensityPlot_5(List1, List2, List3, List4, List5, Label1, Label2, Label3, Lab
     plt.xlabel("Birthweight (Grams)")
     plt.show()
     
+#DESCRIPTIVE STATISTICS FUNCTIONS - individual risks
+def mean_BW(List):
+    mean = round(np.mean(List),1)
+    return mean
+
+def median_BW(List):
+    median = round(np.median(List),1)
+    return median
+
 def correlationCoefficient(x_values, y_values):
     return np.corrcoef(x_values, y_values)
+
+def desStatsTable_2(ListAll, List1, List2, Label1, Label2, title):
+    print('\n',"Descriptive Statistics Data Table of", title, '\n')
+    mothers = 'Number of Mothers', len(ListAll), len(List1), len(List2)
+    means = 'Mean Birth Weight (grams)', mean_BW(ListAll), mean_BW(List1), mean_BW(List2)
+    medians = 'Median Birth Weight (grams)', median_BW(ListAll), median_BW(List1), median_BW(List2)
+    data_rows = [mothers,means, medians]
+    t = Table(rows = data_rows, names = ("Statistic", "All Datapoints",Label1, Label2))
+    print(t)
+#    correlation = correlationCoefficient(x_values, y_values)
+    
+def desStatsTable_3(ListAll, List1, List2, List3, Label1, Label2, Label3, title):
+    print('\n',"Descriptive Statistics Data Table of", title, '\n')
+    mothers = 'Number of Datapoints', len(ListAll), len(List1), len(List2), len(List3)
+    means = 'Mean Birth Weight (grams)', mean_BW(ListAll), mean_BW(List1), mean_BW(List2), mean_BW(List3)
+    medians = 'Median Birth Weight (grams)', median_BW(ListAll), median_BW(List1), median_BW(List2), median_BW(List3)
+    data_rows = [mothers,means, medians]
+    t = Table(rows = data_rows, names = ("Statistic", "All Datapoints",Label1, Label2, Label3))
+    print(t)
+    
+def desStatsTable_5(ListAll, List1, List2, List3, List4, List5, Label1, Label2, Label3, Label4, Label5, title):
+    print('\n',"Descriptive Statistics Data Table of", title, '\n')
+    mothers = 'Number of Datapoints', len(ListAll), len(List1), len(List2), len(List3), len(List4), len(List5)
+    means = 'Mean Birth Weight (grams)', mean_BW(ListAll), mean_BW(List1), mean_BW(List2), mean_BW(List3), mean_BW(List4), mean_BW(List5)
+    medians = 'Median Birth Weight (grams)', median_BW(ListAll), median_BW(List1), median_BW(List2), median_BW(List3), median_BW(List4), median_BW(List5)
+    data_rows = [mothers,means, medians]
+    t = Table(rows = data_rows, names = ("Statistic", "All Datapoints",Label1, Label2, Label3, Label4, Label5))
+    print(t)
--- main.py
+++ main.py
@@ -9,8 +9,8 @@ might want to test this on a classmates computer to be sure it works!"""
 import numpy as np
 import csv
 import matplotlib.pyplot as plt
-from DataParsing import * 
-from Functions import *
+from DataParsing import * #wrong way to import?
+from Functions import * #wrong way to import?
 
 
 # DEMONSTRATION CODE
@@ -25,30 +25,28 @@ print('\n', "Normal Weight of Mother")
 graphNumerical(LWT, BWT, Low, 'Graph of Normal Weight of Mother on Birth Weight')
 
 print('\n', "Number of Premature Births in Medical History")
-graphNumerical(PTL, BWT, Low, 'Graph of Number of Premature Births in Medical History on Birth Weight')
+#graphNumerical(PTL, BWT, Low, 'Graph of Number of Premature Births in Medical History on Birth Weight')
+
+
 print('\n', "Number of Medical Consultations During First Trimester")
-graphNumerical(FTV, BWT, Low, 'Graph of Number of Medical Consultations During First Trimester on Birth Weight')
+#graphNumerical(FTV, BWT, Low, 'Graph of Number of Medical Consultations During First Trimester on Birth Weight')
+desStatsTable_5(BWT, FTV_0, FTV_1, FTV_2, FTV_3, FTV_4, '0 Visits', '1 Visit', '2 Visits', '3 Visits', '4 Visits', 'Data Table of Statistics: Number of Medical Consultations During First Trimester')
 DensityPlot_5(FTV_0, FTV_1, FTV_2, FTV_3, FTV_4, '0 Visits', '1 Visit', '2 Visits', '3 Visits', '4 Visits', 'Density Curve:Number of Medical Consultations During First Trimester' )
 
-print('\n', "CATEGORICAL RISKS as histograms")
 #graphCategorical(LWT, BWT, Low, "Title")
 print('\n', "Race of Mother")
-print("The mean birth weight among mothers in Race 1 is", round(sum(Race_1)/len(Race_1),1), 'grams. The mean birthweight for Race 2 is', round(sum(Race_2)/len(Race_2),2), 'grams. The mean birthweight for Race 3 is', round(sum(Race_3)/len(Race_3),1))
+desStatsTable_3(BWT, Race_1, Race_2, Race_3, 'Race 1', 'Race 2', 'Race 3','Data Table of Statistics: Race of Mother')
 DensityPlot_3(Race_1, Race_2, Race_3, 'Race 1', 'Race 2', 'Race 3', 'Density Curve: Race of Mother')
 
-
-#graphAll(Race, BWT, Low, 'Graph of Race of Mother on Birth Weight')
 print('\n', "Smoking During Pregnancy")
 #Histogram_2(NonSmoker_Birthweights, Smoker_Birthweights, ['Non-Smoker Birthweights', 'Smoker Birthweights'], 8)
-print("The mean birth weight among smokers is", round(sum(Smoker_Birthweights)/len(Smoker_Birthweights),1), 'grams. The mean birthweight among non-smokers is', round(sum(NonSmoker_Birthweights)/len(NonSmoker_Birthweights),1), 'grams.')
+desStatsTable_2(BWT, NonSmoker_Birthweights, Smoker_Birthweights, 'Non-Smokers', 'Smokers','Data Table of Statistics: Smoking During Pregnancy')
 DensityPlot_2(NonSmoker_Birthweights, Smoker_Birthweights, 'Non-Smokers', 'Smokers', 'Density Curve: Smoking During Pregnancy')
 
 print('\n', "Medical History of Hypertension")
-print("The mean birth weight among mothers with a history of Hypertension is", round(sum(HypertensionPos_BW)/len(HypertensionPos_BW),1), 'grams. The mean birthweight among those without a history of Hypertension is', round(sum(HypertensionNeg_BW)/len(HypertensionNeg_BW),1), 'grams.')
-print("There were", len(HypertensionPos_BW), "mothers with a history of hypertension, and", len(HypertensionNeg_BW), "without a history of hypertension.")
+desStatsTable_2(BWT, HypertensionNeg_BW, HypertensionPos_BW, 'No History of Hypertension', 'History of Hypertension','Data Table of Statistics: Hypertension Risk Factor')
 DensityPlot_2(HypertensionNeg_BW, HypertensionPos_BW, 'No History of Hypertension', 'History of Hypertension', 'Density Curve: Hypertension Birthweights')
 
 print('\n', "Uterine Irritability")
-print("The mean birth weight among mothers with a history of Uterine Irritability is", round(sum(UIPos)/len(UIPos),1), "grams. The mean birthweight among those without a history of Uterine Irratibility is", round(sum(UINeg)/len(UINeg),1),'grams.')
+desStatsTable_2(BWT, UIPos, UINeg, 'History of Uterine Irritability', 'No History of Uterine Irratibility', 'Data Table of Statistics: Uterine Irratibility Risk Factor')
 DensityPlot_2(UIPos, UINeg, 'History of Uterine Irritability', 'No History of Uterine Irratibility', 'Density Curve: Uterine Irratibility Birthweights')
-#graphAll(UI, BWT, Low, 'Graph of Uterine Irritability on Birth Weight')
