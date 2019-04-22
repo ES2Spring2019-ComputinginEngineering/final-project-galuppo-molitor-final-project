8eaf93266ff3d04c6f1ccc66611b254c5e2553ee
Victoria Molitor <victoria.molitor@gmail.com>
2019-04-22 11:42:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -20,26 +20,32 @@ print('\n', "The individual correlation between birth weight and each the 9 risk
 print('\n','\n', "NUMERICAL RISKS as scatter plots")
 print('\n', "Age of Mother")
 graphNumerical(Age, BWT, Low, 'Graph of Age on Birth Weight')
-print(correlationCoeddicient(Age, BWT))
+print(correlationCoefficient(Age, BWT))
 print('\n', "Normal Weight of Mother")
 graphNumerical(LWT, BWT, Low, 'Graph of Normal Weight of Mother on Birth Weight')
 
-print('\n', "????")
 print('\n', "Number of Premature Births in Medical History")
 graphNumerical(PTL, BWT, Low, 'Graph of Number of Premature Births in Medical History on Birth Weight')
 print('\n', "Number of Medical Consultations During First Trimester")
 graphNumerical(FTV, BWT, Low, 'Graph of Number of Medical Consultations During First Trimester on Birth Weight')
+DensityPlot_5(FTV_0, FTV_1, FTV_2, FTV_3, FTV_4, '0 Visits', '1 Visit', '2 Visits', '3 Visits', '4 Visits', 'Density Curve:Number of Medical Consultations During First Trimester' )
 
 print('\n', "CATEGORICAL RISKS as histograms")
 #graphCategorical(LWT, BWT, Low, "Title")
-#print('\n', "Race of Mother")
+print('\n', "Race of Mother")
+print("The mean birth weight among mothers in Race 1 is", round(sum(Race_1)/len(Race_1),1), 'grams. The mean birthweight for Race 2 is', round(sum(Race_2)/len(Race_2),2), 'grams. The mean birthweight for Race 3 is', round(sum(Race_3)/len(Race_3),1))
+DensityPlot_3(Race_1, Race_2, Race_3, 'Race 1', 'Race 2', 'Race 3', 'Density Curve: Race of Mother')
+
+
 #graphAll(Race, BWT, Low, 'Graph of Race of Mother on Birth Weight')
 print('\n', "Smoking During Pregnancy")
 #Histogram_2(NonSmoker_Birthweights, Smoker_Birthweights, ['Non-Smoker Birthweights', 'Smoker Birthweights'], 8)
 print("The mean birth weight among smokers is", round(sum(Smoker_Birthweights)/len(Smoker_Birthweights),1), 'grams. The mean birthweight among non-smokers is', round(sum(NonSmoker_Birthweights)/len(NonSmoker_Birthweights),1), 'grams.')
-DensityPlot(NonSmoker_Birthweights, Smoker_Birthweights, 'Non-Smokers', 'Smokers', 'Density Curve: Smoking During Pregnancy')
+DensityPlot_2(NonSmoker_Birthweights, Smoker_Birthweights, 'Non-Smokers', 'Smokers', 'Density Curve: Smoking During Pregnancy')
+
 print('\n', "Medical History of Hypertension")
-DensityPlot(HypertensionNeg_BW, HypertensionPos_BW, 'No History of Hypertension', 'History of Hypertension', 'Density Curve: Hypertension Birthweights')
-#graphAll(Hypertension, BWT, Low, 'Graph of Medical History of Hypertension on Birth Weight')
+print("The mean birth weight among mothers with a history of Hypertension is", round(sum(HypertensionPos_BW)/len(HypertensionPos_BW),1), 'grams. The mean birthweight among those without a history of Hypertension is', round(sum(HypertensionNeg_BW)/len(HypertensionNeg_BW),1), 'grams.')
+DensityPlot_2(HypertensionNeg_BW, HypertensionPos_BW, 'No History of Hypertension', 'History of Hypertension', 'Density Curve: Hypertension Birthweights')
+
 #print('\n', "Uterine Irritability")
 #graphAll(UI, BWT, Low, 'Graph of Uterine Irritability on Birth Weight')
