c1f92ead6a93b79781ad5c5971b5d6b4dba13550
Victoria Molitor <victoria.molitor@gmail.com>
2019-04-23 15:42:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from Functions import * #wrong way to import?
 
 
 # DEMONSTRATION CODE
-print("There are nine different risk factors observed in this dataset that may have an effect on the chances of a low birth weight")
+print("There are eight different risk factors observed in this dataset that may have an effect on the chances of a low birth weight")
 print('\n', "The individual correlation between birth weight and each the 9 risk factors is demonstrated in the following:")
 
 print('\n','\n', "NUMERICAL RISKS as scatter plots")
@@ -25,6 +25,7 @@ print('\n', "Normal Weight of Mother")
 graphNumerical(LWT, BWT, Low, 'Graph of Normal Weight of Mother on Birth Weight')
 
 print('\n', "Number of Premature Births in Medical History")
+DensityPlot_4(PTL_0, PTL_1, PTL_2, PTL_3, '0 Premature Births', '1 Premature Birth', '2 Premature Births', '3 Premature Births', 'Density Curve: Number of Premature Births in Medical History')
 #graphNumerical(PTL, BWT, Low, 'Graph of Number of Premature Births in Medical History on Birth Weight')
 
 
@@ -37,16 +38,25 @@ DensityPlot_5(FTV_0, FTV_1, FTV_2, FTV_3, FTV_4, '0 Visits', '1 Visit', '2 Visit
 print('\n', "Race of Mother")
 desStatsTable_3(BWT, Race_1, Race_2, Race_3, 'Race 1', 'Race 2', 'Race 3','Data Table of Statistics: Race of Mother')
 DensityPlot_3(Race_1, Race_2, Race_3, 'Race 1', 'Race 2', 'Race 3', 'Density Curve: Race of Mother')
+print("For Race 1:")
+T_Test_Print(Race_1, Race_2+Race_3)
+print("For Race 2:")
+T_Test_Print(Race_2, Race_1+Race_3)
+print("For Race 3:")
+T_Test_Print(Race_3, Race_1+Race_2)
 
 print('\n', "Smoking During Pregnancy")
 #Histogram_2(NonSmoker_Birthweights, Smoker_Birthweights, ['Non-Smoker Birthweights', 'Smoker Birthweights'], 8)
 desStatsTable_2(BWT, NonSmoker_Birthweights, Smoker_Birthweights, 'Non-Smokers', 'Smokers','Data Table of Statistics: Smoking During Pregnancy')
 DensityPlot_2(NonSmoker_Birthweights, Smoker_Birthweights, 'Non-Smokers', 'Smokers', 'Density Curve: Smoking During Pregnancy')
+T_Test_Print(NonSmoker_Birthweights, Smoker_Birthweights)
 
 print('\n', "Medical History of Hypertension")
 desStatsTable_2(BWT, HypertensionNeg_BW, HypertensionPos_BW, 'No History of Hypertension', 'History of Hypertension','Data Table of Statistics: Hypertension Risk Factor')
 DensityPlot_2(HypertensionNeg_BW, HypertensionPos_BW, 'No History of Hypertension', 'History of Hypertension', 'Density Curve: Hypertension Birthweights')
+T_Test_Print(HypertensionNeg_BW, HypertensionPos_BW)
 
 print('\n', "Uterine Irritability")
 desStatsTable_2(BWT, UIPos, UINeg, 'History of Uterine Irritability', 'No History of Uterine Irratibility', 'Data Table of Statistics: Uterine Irratibility Risk Factor')
 DensityPlot_2(UIPos, UINeg, 'History of Uterine Irritability', 'No History of Uterine Irratibility', 'Density Curve: Uterine Irratibility Birthweights')
+T_Test_Print(UINeg, UIPos)
